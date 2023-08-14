9d0e186b55216f0e17e8e980dcfa1cb5715769a9
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-14 23:06:53 +0530

    Update flatland space station.c

--- flatland space station.c
+++ flatland space station.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
+
+int workbook(int n, int k, int* arr) {
+    int page = 1; // Page number
+    int specialProblems = 0; // Count of special problems
+    
+    for (int i = 0; i < n; i++) {
+        for (int problem = 1; problem <= arr[i]; problem++) {
+            if (problem == page) {
+                specialProblems++;
+            }
+            if (problem % k == 0 || problem == arr[i]) {
+                page++;
+            }
+        }
+    }
+    
+    return specialProblems;
+}
+
+int main() {
+    int n, k;
+    scanf("%d %d", &n, &k);
+    
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int result = workbook(n, k, arr);
+    printf("%d\n", result);
+    
+    return 0;
+}
 
