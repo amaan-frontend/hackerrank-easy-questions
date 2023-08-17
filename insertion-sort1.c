72a178dc6f54a9c8a807d6ee17b4aa8c31b4b3d9
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-17 23:38:13 +0530

    Update insertion-sort1.c

--- insertion-sort1.c
+++ insertion-sort1.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertionSort1(int n, int arr[]) {
+    int key = arr[n - 1];
+    int i = n - 2;
+
+    while (i >= 0 && arr[i] > key) {
+        arr[i + 1] = arr[i];
+        printArray(arr, n);
+        i--;
+    }
+
+    arr[i + 1] = key;
+    printArray(arr, n);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort1(n, arr);
+
+    return 0;
+}
 
