29355544f4de35e7b9d1e621b7ed82fb5c8677a4
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-18 23:02:30 +0530

    Update minimum-distances.c

--- minimum-distances.c
+++ minimum-distances.c
@@ -1,1 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int minimumDistances(int a_count, int* a) {
+    int minDistance = a_count; // Initialize with a large value
+    for (int i = 0; i < a_count; i++) {
+        for (int j = i + 1; j < a_count; j++) {
+            if (a[i] == a[j]) {
+                int distance = j - i;
+                if (distance < minDistance) {
+                    minDistance = distance;
+                }
+            }
+        }
+    }
+    
+    if (minDistance == a_count) {
+        return -1; // No matching elements found
+    } else {
+        return minDistance;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int* arr = (int*)malloc(n * sizeof(int));
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    int result = minimumDistances(n, arr);
+    printf("%d\n", result);
+    
+    free(arr);
+    
+    return 0;
+}
 
