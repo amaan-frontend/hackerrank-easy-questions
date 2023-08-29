5fca53129046dd3c4742d461701e2d0f265a8f9d
frontender <134868734+amaan-frontend@users.noreply.github.com>
2023-08-29 23:45:25 +0530

    Update strange-code.c

--- strange-code.c
+++ strange-code.c
@@ -1,1 +1,29 @@
 
+#include <stdio.h>
+#include <math.h>
+
+long strangeCounter(long t) {
+    // Find the cycle in which t falls
+    long cycle = 0;
+    while (3 * (pow(2, cycle) - 1) < t) {
+        cycle++;
+    }
+    
+    // Calculate the initial value of the current cycle
+    long initialValue = 3 * (pow(2, cycle) - 1);
+    
+    // Calculate the value displayed at time t
+    long value = initialValue - (t - (initialValue + 1));
+    
+    return value;
+}
+
+int main() {
+    long t;
+    scanf("%ld", &t);
+    
+    long result = strangeCounter(t);
+    printf("%ld\n", result);
+    
+    return 0;
+}
